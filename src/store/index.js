b24d8ee152bb8db407835ed35ce143afdb76f630
lienht-1767 <hoang.thi.lien@sun-asterisk.com>
2019-12-24 14:57:07 +0700

    add redux saga

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,15 @@
 import { createStore, compose, applyMiddleware } from "redux";
+import createSagaMiddleware from "redux-saga";
 import rootReducer from "../reducers";
+import rootSaga from './store/rootSaga';
+
 const reduxDevTools =
   (window.__REDUX_DEVTOOLS_EXTENSION__ &&
     window.__REDUX_DEVTOOLS_EXTENSION__()) ||
   compose;
 
-let store = createStore(rootReducer, compose(applyMiddleware(), reduxDevTools));
+const sagaMiddleware = createSagaMiddleware()
+sagaMiddleware.run(rootSaga)
+
+let store = createStore(rootReducer, compose(applyMiddleware(sagaMiddleware), reduxDevTools));
 export default store;
